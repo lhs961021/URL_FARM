c2bf18fe557de9eb136b77c4bd2405c5ad84a40b
이태검 <taegeom0601@gmail.com>
2021-11-15 20:25:28 +0900

    방 생성 페이지 디자인

--- analyze/models.py
+++ analyze/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class URLAnalyze(models.Model):
@@ -7,3 +7,5 @@ class URLAnalyze(models.Model):
     title= models.TextField(null=True)
     content=models.TextField(null=True)
     check=models.IntegerField(default=0)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    
